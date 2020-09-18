bfd89e59becbf27ff5e88f6172db09c9da3f88e5
Alessandro <alessandro.boscato@gmail.com>
2020-09-18 15:10:58 +0200

    Base html/css + handlebars

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(
+  function() {
+    // Creiamo un calendario dinamico con le festività.
+    // Il calendario partirà da gennaio 2018 e si concluderà a dicembre 2018 (unici dati disponibili sull’API).
+    // Milestone 1
+    // Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
+    // API: https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0
+    var source = document.getElementById("entry-template").innerHTML;
+    var template = Handlebars.compile(source);
+    var context = {
+      "numero": 1,
+      "mese": "Gennaio",
+      "anno": 2018,
+      "festività": "Capodanno"
+    };
+    var html = template(context);
+    $("#calendar").append(html);
+
+
+
+  });
