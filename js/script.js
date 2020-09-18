ca278cbef9b7cd828a3ab40cfcb8aed18e92578b
Alessandro <alessandro.boscato@gmail.com>
2020-09-18 16:20:31 +0200

    Commit

--- js/script.js
+++ js/script.js
@@ -5,38 +5,39 @@ $(document).ready(
     // Milestone 1
     // Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
     // API: https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0
-    var startDate = moment("01-01-2018", "DD-MM-YYYY");
+    var startDate = moment("2018-01-01");
     console.log(startDate);
     var daysInMonth = startDate.daysInMonth();
     console.log(daysInMonth);
-
+    var counter = startDate._i;
+    console.log(counter);
+    // compilo il template handlebars
     var source = document.getElementById("entry-template").innerHTML;
     var template = Handlebars.compile(source);
-    for (var i = 1; i < daysInMonth + 1; i++) {
 
-      var context = {
-        "numero": i,
-        "mese": "Gennaio",
-        "anno": 2018,
-        "festività": "Capodanno"
-      };
-      var html = template(context);
-      $("#calendar").append(html);
-    }
-
+    // chiamata ajax
     $.ajax(
       {
       "url": "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
       "method": "GET",
       "success": function (data) {
-      console.log(data.response);
+        console.log(data.response);
+        //renderizzo handlebars
+        for (var i = 1; i < daysInMonth + 1; i++) {
+          var counter = startDate._i;
+          var context = {
+            "numero": i,
+            "mese": startDate.format("MMMM")
+            
+          };
+          var html = template(context);
+          $("#calendar").append(html);
+        }
+
       },
       "error": function (richiesta, stato, errori) {
       alert("E' avvenuto un errore. " + errore);
       }
       });
-
-
-
 
   });
