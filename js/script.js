651ddbc3a1fca671e9b53fd6710c2dfe284b7a54
Alessandro <alessandro.boscato@gmail.com>
2020-09-18 15:38:14 +0200

    chiamata ajax

--- js/script.js
+++ js/script.js
@@ -5,16 +5,37 @@ $(document).ready(
     // Milestone 1
     // Creiamo il mese di Gennaio, e con la chiamata all'API inseriamo le festività.
     // API: https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0
+    var startDate = moment("01-01-2018", "DD-MM-YYYY");
+    console.log(startDate);
+    var daysInMonth = startDate.daysInMonth();
+    console.log(daysInMonth);
+
     var source = document.getElementById("entry-template").innerHTML;
     var template = Handlebars.compile(source);
-    var context = {
-      "numero": 1,
-      "mese": "Gennaio",
-      "anno": 2018,
-      "festività": "Capodanno"
-    };
-    var html = template(context);
-    $("#calendar").append(html);
+    for (var i = 1; i < daysInMonth + 1; i++) {
+
+      var context = {
+        "numero": i,
+        "mese": "Gennaio",
+        "anno": 2018,
+        "festività": "Capodanno"
+      };
+      var html = template(context);
+      $("#calendar").append(html);
+    }
+
+    $.ajax(
+      {
+      "url": "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
+      "method": "GET",
+      "success": function (data) {
+      console.log(data.response);
+      },
+      "error": function (richiesta, stato, errori) {
+      alert("E' avvenuto un errore. " + errore);
+      }
+      });
+
 
 
 
